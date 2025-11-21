2602001fccc31497223ecc265390ef0ccf483661
Andares Mer <andares@foxmail.com>
2025-11-21 15:04:37 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import { Gauldoth } from '~/encryption';
+import { BaseFlow, Obfus } from '~/generators';
 import * as Utils from '~/utils';
 
 // 具名导出（零散形式）
 export {
   Gauldoth,
+  BaseFlow,
+  Obfus,
   Utils,
 };
 
 // 默认导出聚合对象
 export default {
   Gauldoth,
+  BaseFlow,
+  Obfus,
   Utils,
 };
