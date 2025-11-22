a11762c246c23f945762a30ebc3e6bed6768dbf1
Andares Mer <andares@foxmail.com>
2025-11-22 16:20:40 +0800

    export CryptoManager Function

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { Gauldoth } from '~/encryption';
+import { Gauldoth, CryptoManager } from '~/encryption';
 import { BaseFlow, Obfus } from '~/generators';
 import * as Utils from '~/utils';
 
 // 具名导出（零散形式）
 export {
   Gauldoth,
+  CryptoManager,
   BaseFlow,
   Obfus,
   Utils,
@@ -13,6 +14,7 @@ export {
 // 默认导出聚合对象
 export default {
   Gauldoth,
+  CryptoManager,
   BaseFlow,
   Obfus,
   Utils,
